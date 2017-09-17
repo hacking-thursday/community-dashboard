8aad4d219bcce480c4154e6213cd6919b6770128
Kuo-tung Kao <ya790206@gmail.com>
2017-09-17 23:20:14 +0800

    use fuse.js to search

--- community-dashboard-frontend/src/app/searchpanel/component.ts
+++ community-dashboard-frontend/src/app/searchpanel/component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 
 import { MeetupService } from '../services/meetupService'
 
+import * as Fuse from 'fuse.js'
 
 @Component({
   selector: 'app-total-event-per-week-component',
@@ -11,7 +12,9 @@ export class SearchPanel implements OnInit {
   public results = []
 
   raw = {}
-  
+  data = []
+  fuse: Fuse = null
+
   @ViewChild('keywordInput') keywordInput: any
   @ViewChild('hiddenElement') hiddenElement: any
 
@@ -25,35 +28,67 @@ export class SearchPanel implements OnInit {
 
   }
 
-  search(key: string): any {
-    this.meetupService.getEvents().then(data => {
-      this.raw = data
+  createIndex() {
+    const options = {
+      shouldSort: true,
+      tokenize: true,
+      includeScore: true,
+      threshold: 0.6,
+      location: 0,
+      distance: 100,
+      maxPatternLength: 32,
+      minMatchCharLength: 2,
+      matchAllTokens: true,
+      includeMatches: true,
+      keys: [
+        {name: 'group', weight: 0.7},
+        {name: 'text', weight: 0.3}
+      ],
+      id: 'index'
+    }
+    this.meetupService.getEvents().then(raw => {
+      this.raw = raw
       this.results = []
-      const re = new RegExp("\\W" + key + "\\W", "g");
-      for (const i of this.raw['groups']){
-        if (i['events'].length > 0){
-            for (const e of i['events']){
-              if (!e.hasOwnProperty('text')) {
-                this.hiddenElement.nativeElement.innerHTML = e['description']
-                e['text'] = this.hiddenElement.nativeElement.innerText.toLowerCase()
-              }
-              e['group'] = i['name']
-              var searchResult = re.exec(e['text'])
-              if (searchResult != null) {
-                var startIndex = re.lastIndex - key.length 
-                if (startIndex < 0){
-                  startIndex = 0
-                }
-                e['short'] = e['text'].slice(startIndex, re.lastIndex + 200)
-                this.results.push(e)
-              }
+      let index = 0
+      for (const i of this.raw['groups']) {
+        if (i['events'].length > 0) {
+          for (const e of i['events']) {
+            if (!e.hasOwnProperty('text')) {
+              this.hiddenElement.nativeElement.innerHTML = e['description']
+              e['text'] = this.hiddenElement.nativeElement.innerText.toLowerCase()
             }
+            e['group'] = i['name']
+            e['index'] = index
+            index += 1
+            this.data.push(e)
+          }
         }
       }
+      this.fuse = new Fuse(this.data, options)
     })
   }
 
+  search(key: string): any {
+    if (this.fuse === null) {
+      this.createIndex()
+    } else {
+      const fuseOutput = this.fuse.search(key)
+      console.log(fuseOutput)
+      this.results.length = 0
+      for (const i of fuseOutput) {
+        let e = this.data[parseInt(i['item'], 10)]
+        if (i['matches'].length > 0) {
+          e['short'] = i['matches'][0]['value']
+        }else{
+          e['short'] = ''
+        }
+        this.results.push(e)
+      }
+    }
+  }
+
   ngOnInit(): void {
+    this.createIndex()
   }
 
   // events
